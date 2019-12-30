65b4efb36367c348f9a44ee76b9a920f99f0f393
Takumi Ito <ito.tkmi@gmail.com>
2019-12-30 14:16:17 +0900

    Print margin - Change initial tab to space

--- lib/print.ex
+++ lib/print.ex
@@ -8,7 +8,7 @@ defmodule Later.Print do
 
   def main(nil) do
     IO.puts(" ")
-    IO.puts("Todo not found.")
+    IO.puts(" Todo not found.")
     IO.puts(" ")
 
     :ok
@@ -16,7 +16,7 @@ defmodule Later.Print do
 
   def main([]) do
     IO.puts(" ")
-    IO.puts("Your list is empty.")
+    IO.puts(" Your list is empty.")
     IO.puts(" ")
 
     :ok
@@ -24,7 +24,7 @@ defmodule Later.Print do
 
   def main(todos) when is_list(todos) do
     IO.puts(" ")
-    IO.puts("#{@tab}#{ansi_bright()}Todos#{ansi_normal()}")
+    IO.puts(" #{ansi_bright()}Todos#{ansi_normal()}")
     IO.puts(" ")
     Enum.each(todos, fn t -> print_todo(t) end)
     IO.puts(" ")
@@ -47,13 +47,13 @@ defmodule Later.Print do
 
   def help() do
     IO.puts(" ")
-    IO.puts("#{@tab}#{ansi_bright()}#{@help_title}#{ansi_normal()}")
-    IO.puts("#{@tab}#{@help_description}")
+    IO.puts(" #{ansi_bright()}#{@help_title}#{ansi_normal()}")
+    IO.puts(" #{@help_description}")
     IO.puts(" ")
-    IO.puts("#{@tab}#{ansi_bright()}Repository#{ansi_normal()}")
-    IO.puts("#{@tab}#{@repo_link}")
+    IO.puts(" #{ansi_bright()}Repository#{ansi_normal()}")
+    IO.puts(" #{@repo_link}")
     IO.puts(" ")
-    IO.puts("#{@tab}#{ansi_bright()}Commands#{ansi_normal()}")
+    IO.puts(" #{ansi_bright()}Commands#{ansi_normal()}")
     Enum.each(Commands.get_commands(), fn command -> print_help(command) end)
     IO.puts(" ")
 
@@ -62,14 +62,14 @@ defmodule Later.Print do
 
   def error(args) do
     IO.puts(" ")
-    IO.puts("#{@tab}#{args.reason}")
+    IO.puts(" #{args.reason}")
     IO.puts(" ")
 
     :ok
   end
 
   defp print_todo(todo),
-    do: IO.puts("#{@tab}#{todo.id}. #{ansi_bright()}#{todo.title}#{ansi_normal()}")
+    do: IO.puts(" #{todo.id}. #{ansi_bright()}#{todo.title}#{ansi_normal()}")
 
   defp print_help(command) do
     command_name = "#{ansi_bright()}· #{command.name}#{ansi_normal()}"
@@ -80,16 +80,16 @@ defmodule Later.Print do
     command_parameters = command.parameters
 
     IO.puts(" ")
-    IO.puts("#{@tab}#{@tab}#{command_name} #{command_alias}")
-    IO.puts("#{@tab}#{@tab}#{ansi_faint()}#{command_description}#{ansi_normal()}")
+    IO.puts(" #{@tab}#{command_name} #{command_alias}")
+    IO.puts(" #{@tab}#{ansi_faint()}#{command_description}#{ansi_normal()}")
 
     if command_parameters do
       IO.puts(" ")
-      IO.puts("#{@tab}#{@tab}#{@tab}#{ansi_bright()}parameters#{ansi_normal()}")
+      IO.puts(" #{@tab}#{@tab}#{ansi_bright()}parameters#{ansi_normal()}")
 
       Enum.each(command_parameters, fn param ->
         msg =
-          "#{@tab}#{@tab}#{@tab}#{param.name} #{
+          " #{@tab}#{@tab}#{param.name} #{
             "#{ansi_faint()}(#{param.type})#{ansi_normal()}#{
               if param.required, do: "#{ansi_faint()} (required)#{ansi_normal()}"
             }"
@@ -103,14 +103,14 @@ defmodule Later.Print do
       IO.puts(" ")
 
       IO.puts(
-        "#{@tab}#{@tab}#{@tab}#{ansi_bright()}#{
+        " #{@tab}#{@tab}#{ansi_bright()}#{
           if Enum.count(command_examples) <= 1, do: "example", else: "examples"
         }#{ansi_normal()}"
       )
 
       Enum.each(command_examples, fn example ->
         IO.puts(
-          "#{@tab}#{@tab}#{@tab}#{example.command} #{ansi_faint()}# #{example.comment}#{
+          " #{@tab}#{@tab}#{example.command} #{ansi_faint()}# #{example.comment}#{
             ansi_normal()
           }"
         )
